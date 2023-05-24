567a45e4c6adaac37b889aeea788d3af5eaea624
poleskoea <poleskoea@gmail.com>
2023-05-24 23:17:34 +0700

    added homework scope

--- Cycles/cycles.js
+++ Cycles/cycles.js
@@ -1,17 +1,19 @@
 // Выведите все четные числа до 10 включительно. ⭐не используйте оператор **continue**.
-for (let i = 0; i <= 10; i += 2) {
-  console.log(i);
-}
+// for (let i = 0; i <= 10; i += 2) {
+//   console.log(i);
+// }
 
-// Создайте бесконечный цикл и прервите его на 5ой итерации.
-let i = 0;
-while (true) {
-  i++;
-  console.log(`Iteration ${i}`);
-  if (i === 5) break;
-}
+// // Создайте бесконечный цикл и прервите его на 5ой итерации.
+// let i = 0;
+// while (true) {
+//   i++;
+//   console.log(`Iteration ${i}`);
+//   if (i === 5) break;
+// }
 
-// Создайте массив блюд, с названием, ингредиентами, ценой приготовления и ценой реализации. (прямо как в уроке номер 8) Посчитайте с помощью цикла профит для каждого блюда и запишите его в объект блюда. ⭐Не задавайте цену приготовления сразу. Задайте массив ингредиентов. Отдельно создайте объект хранящий стоимость этих ингредиентов. Посчитайте стоимость приготовления динамически (с помощью цикла)
+// global variable
+let menu = "";
+
 let sushi = {
   dishName: "Sushi",
   ingredients: ["salmon", "rice", "salt", "nori", "water"],
@@ -65,23 +67,36 @@ const countCost = function (dish) {
     dish.cost += ingrPrices[ingr];
   }
 };
-for (dish of foodArray) countCost(dish);
-// countCost(sagudai);
-// countCost(sushi);
-// countCost(borsch);
-// countCost(pancakes);
-console.log(foodArray);
 
-let totalProfit = 0;
+for (dish of foodArray) {
+  countCost(dish);
+  dish.profit = dish.price - dish.cost;
+  // локальная переменная с инфо о блюде
+  const dishInfo = `${dish.dishName}`.padEnd(30, ".") + `${dish.price}₽\n`;
+  // добавление в глобальную переменную меню
+  menu += dishInfo;
+}
+
+const totalProfit = foodArray
+  .map((dish) => dish.profit)
+  .reduce((acc, profit) => acc + profit);
+
+console.log(foodArray);
+console.log(menu);
+console.log(totalProfit);
+
+// let totalProfit = 0;
 // for (let i = 0; i < foodArray.length; i++) {
 //   const profit = foodArray[i].price - foodArray[i].cost;
 //   console.log(`Profit from ${foodArray[i].dishName} : ${profit}`);
 //   totalProfit += profit;
 // }
 
-foodArray.forEach((dish) => {
-  const profit = dish.price - dish.cost;
-  console.log(`Profit from ${dish.dishName} : ${profit}`);
-  totalProfit += profit;
-});
-console.log(`Total profit: ${totalProfit}`);
+// foodArray.forEach((dish) => {
+//   profit = dish.price - dish.cost;
+//   // console.log(`Profit from ${dish.dishName} : ${profit}`);
+//   totalProfit += profit;
+//   const dishInfo = `${dish.dishName}`.padEnd(30, ".") + `${dish.price}₽\n`;
+//   menu += dishInfo;
+// });
+// console.log(`Total profit: ${totalProfit}`);
