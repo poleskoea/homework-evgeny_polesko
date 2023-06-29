61ab5309405ec7a162d6cec9a2c263d443b6b8e6
poleskoea <poleskoea@gmail.com>
2023-06-29 21:55:05 +0700

    added homework array methods

---
+++ Array methods/arrayMethods.js
@@ -0,0 +1,82 @@
+let sushi = {
+  dishName: "Sushi",
+  ingredients: ["salmon", "rice", "salt", "nori", "water"],
+  price: 450,
+};
+
+let pancakes = {
+  dishName: "Pancakes",
+  ingredients: ["milk", "egg", "flour", "sugar", "salt"],
+  price: 250,
+};
+
+let borsch = {
+  dishName: "Borsch",
+  ingredients: ["meat", "cabbage", "beetroot", "carrot", "water", "tomato"],
+  price: 270,
+};
+
+let sagudai = {
+  dishName: "Sagudai",
+  ingredients: ["omul", "salt", "oil", "onion", "vinegar"],
+  price: 300,
+};
+const foodArray = [sushi, pancakes, borsch, sagudai];
+
+const ingrPrices = {
+  omul: 190,
+  salt: 10,
+  oil: 20,
+  onion: 15,
+  vinegar: 15,
+  carrot: 7,
+  tomato: 10,
+  beetroot: 12,
+  water: 0,
+  cabbage: 16,
+  meat: 130,
+  sugar: 20,
+  flour: 40,
+  egg: 70,
+  milk: 20,
+  salmon: 240,
+  rice: 50,
+  nori: 60,
+};
+
+// Перепишите цикл, который считает себестоимость блюда из прошлых заданий на функцию, которая использует `reduce` .
+
+const countCost = function (dish, prices) {
+  return dish.ingredients.reduce((cost, ingr) => (cost += prices[ingr]), 0);
+};
+console.log(countCost(pancakes, ingrPrices));
+
+// Используйте `map` , чтобы получить массив с объектами в которых содержится только название и стоимость каждого блюда.
+const nameAndPrice = foodArray.map((dish) => {
+  //   return ({ dishName, price } = dish), { dishName, price };
+  return (({ dishName, price }) => ({ dishName, price }))(dish);
+});
+console.log(nameAndPrice);
+console.log(foodArray);
+
+// Определите, есть ли в меню хоть одно вегетарианское блюдо, используя `some`
+const checkVegDish = function (dish) {
+  if (
+    !dish.ingredients.includes("meat") &&
+    !dish.ingredients.includes("salmon") &&
+    !dish.ingredients.includes("omul")
+  )
+    return true;
+  else return false;
+};
+
+const haveVegDish = foodArray.some((dish) => checkVegDish(dish));
+console.log(haveVegDish);
+
+// Определите, полностью ли у вас вегетарианское меню с помощью `every`.
+const vegetarianMenuOnly = foodArray.every((dish) => checkVegDish(dish));
+console.log(vegetarianMenuOnly);
+
+// Создайте массив с вегетарианскими блюдами с помощью filter.
+const vegMenu = foodArray.filter((dish) => checkVegDish(dish));
+console.log(vegMenu);
